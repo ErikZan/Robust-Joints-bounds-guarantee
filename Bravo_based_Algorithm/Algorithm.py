f05b0225dfe5c864ade82d3479923be2157cb2cb
Erik <erikzanolli@gmail.com>
2020-12-31 12:23:16 +0100

    Simple Joints and some function implemented
    still not properly working

--- Bravo_based_Algorithm/Algorithm.py
+++ Bravo_based_Algorithm/Algorithm.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from Bravo_based_Algorithm.Functions import  compute_torque
+from Bravo_based_Algorithm.Functions import  rejected_point,compute_torque, remove_old_add_new, remove_old_add_new_if_P,Approved_point
 import pinocchio as se3
 from pinocchio.utils import *
 import os
@@ -19,19 +19,20 @@ import matplotlib.patches as mpatches
 import datetime
 import itertools
 import Functions
+from pylab import *
 
 # initialize lists
-L=np.array([[0.0,0.0]]); # contains all the points that enter in the algorithm
-P=np.array([[0.0,0.0]]); # contains all the points positicely evaluated by the algorithm
-R=np.array([[0.0,0.0]]); # contains all the points rejected by the algorithm
+L=[];                               # contains all the points that enter in the algorithm
+P=[(0.0,0.0)]; # contains all the points positicely evaluated by the algorithm
+R=[];                      # contains all the points rejected by the algorithm
 
 # initialize bound on states
 
-X=np.array([[-0.5,0.5],[-0.1,0.1]]); # position and velocity bound
-U=np.array([[-80.0,80.0]]); # torque bounds
+X=np.array([[-0.5,0.5],[-0.5,0.5]]);    # position and velocity bound
+U=np.array([[-80.0,80.0]]);             # torque bounds
 
 # Data of the system 
-dt=0.1;
+dt=0.05;
 M=1.5;
 g=9.81;
 l=1.5;
@@ -40,13 +41,15 @@ tau_min=-50;
 
 # definition of the initial omega
 
-omega=np.array([0.0])
+omega=np.array([0.0]) # not yet used
 
 # starting points in our L list
 # defined as the extremes verteces of our X-region
 
 tmp_L=list(itertools.product(X.tolist()[0],X.tolist()[1]))
-L=np.array(tmp_L); 
+L=tmp_L
+#L=[(0.5,0.5)]
+#L=np.array(tmp_L); 
 print('\n initial starting points in the list : \n')
 print(L,'\n');
 
@@ -56,24 +59,86 @@ print(L,'\n');
 ###############################################################
 
 # Non linearity for a simple pendulum system
+# Simple joint with nl effect
 def nl_effect_pendulum(q,dq):
-    nl= -M*g*l*sin(q)+M*l**2*dq/dt;
+    nl= -M*g*l*sin(q)*0.0+M*l**2*dq/dt; # Is it correct ???
     return nl;
 
-q=0
-for i in range(len(L)):
-    nonlinear= nl_effect_pendulum(L[i][0],L[i][1]);
-    r=compute_torque(X,U,L[i],dt,nonlinear,M)
-    print(r.x);
+'''
+We check if the elements is L can reach the desired conditions in one time-step,
+if they we put in P, if not we reject the points in R. we choose the point using
+compute_torque().
+The perfect exit condition for this loop is to empty the L checking the distance 
+between two consecutive generated points, but is not yet implemented
+'''
+i=0
+while (L !=[] and i<=1000):
+      
+    nonlinear = nl_effect_pendulum(L[0][0],L[0][1]);
+    print('non linear effect: ',nonlinear)
     
-    if (r.x > tau_max or r.x <tau_min):
-        if (abs(L[i][0]) >= abs(L[i][1])):
-            #for q in range(len(L)):
-                # if (L[i][0]==abs(L[q][0]):
-                #     q=q
-                #     # must select properly
-            b = np.array([[L[i][0]/2.0,L[i][1]]])
-            L=np.concatenate((L,b))
-        L[i]=[0,0]
+    r=compute_torque(X,U,L[0],dt,nonlinear,M)
+    print('\n computed tau',r.x);
+    
+    i=i+1
+    
+    if (r.x >= tau_max or r.x <= tau_min):
+        if (L[0] in R):
+            R
+        else:
+            R.append(L[0])
+            
+        #remove_old_add_new(L[0],L,P,X)
+        rejected_point(L[0],L,P,X)
+                    
+    else:
+        if (L[0] in P):
+            P
+        else:
+            P.append(L[0])
+        
+        #remove_old_add_new_if_P(L[0],L,R,X)
+        Approved_point(L[0],L,R,X)
+        
+        # if ( (0.0,0.0) in P):
+        #     P.remove((0.0,0.0))
+        # L.remove(L[0])
+        # if (abs(L[i][0]) >= abs(L[i][1])):
+        #     #for q in range(len(L)):
+        #         # if (L[i][0]==abs(L[q][0]):
+        #         #     q=q
+        #         #     # must select properly
+        #     b = np.array([[L[i][0]/2.0,L[i][1]]])
+        #     L=np.concatenate((L,b))
+        # L[i]=[0,0]
 
-print(L)
+print('this is L \n',L,'\n')
+print('this is R \n',R,'\n')
+print('this is P \n',P,'\n')
+
+(f,ax) = plut.create_empty_figure(1)
+eps=1E-2
+ax.set_ylim([-0.5-eps,0.5+eps])
+ax.set_xlim([-0.5-eps,0.5+eps])
+
+
+if(L != [] ):
+    LL=np.array(L)
+    scatter(LL[:,0],LL[:,1],color="blue")
+
+if(R != [] ):
+    RR=np.array(R)
+    scatter(RR[:,0],RR[:,1],color="red")
+    
+if(P != [] ):
+    PP=np.array(P)
+    scatter(PP[:,0],PP[:,1],color="green")
+LW=1
+ax.plot([-0.5, 0.5], [-0.1, -0.1], 'r--',linewidth=LW);
+ax.plot([-0.5, 0.5], [0.1, 0.1], 'r--',linewidth=LW);
+ax.plot([-0.5, -0.5], [-0.1, 0.1], 'r--',linewidth=LW);
+ax.plot([0.5, 0.5], [-0.1, 0.1], 'r--',linewidth=LW);
+
+
+    
+plt.show()
--- Bravo_based_Algorithm/Functions.py
+++ Bravo_based_Algorithm/Functions.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from numpy.lib.financial import ppmt
 import pinocchio as se3
 from pinocchio.utils import *
 import os
@@ -30,43 +31,307 @@ def compute_torque(X,U,L_point,dt,nonlinear,M):
     
     PROBLEMS:
     -Contemporary design suitable only for stopping the joint in one timestep:
-    NOT FLEXIBLE
+    NOT FLEXIBLE -> to do
     -No continuos-time-guarantee of the satisfaction of the position limits:
     i.e. if It starts from the lower limits with negative velocity limits is 
-    violated in continuos while not being warned from the algorithm
-    COMPUTE MAXIMUM IN POSITION
+    violated in continuos while not being warned from the algorithm:
+    COMPUTE MAXIMUM IN POSITION -> added pos_middle_position() that mitigate 
+    but does not solve the the problem
     '''
     q=L_point[0]
     dq=L_point[1]
     qmax=X[0][1]
     qmin=X[0][0]
-    
+    EPS =1E-06
+    print(q,dq,qmax,qmin)
     def cost_fun(tau): # FLEXIBILITY: can i add another term to taking account of the previous step?
-        cost = dq +dt*(tau+nonlinear)
+        cost = dq +dt*(tau+nonlinear) 
         return cost**2
 
-    def pos_upper_bound(tau): # No continuous time bound satisfaction!       
-        position =q+dt*dq+dt**2 * ((tau+nonlinear)/M)/2 ;
-        return position+qmax
+    # def pos_upper_bound(tau): # No continuous time bound satisfaction!       
+    #     position =q+dt*dq+dt**2 * ((tau+nonlinear)/M)/2 ;
+    #     return position+qmax
 
     def pos_lower_bound(tau):        
         position = q+dt*dq+dt**2 * ((tau+nonlinear)/M)/2 ;
-        return position+qmin
+        return -abs(position)+qmax
+    
+    def pos_middle_position(tau):
+        position = q+dt/2*dq+(dt/2)**2 * ((tau+nonlinear)/M)/2 ;
+        return -abs(position)+qmax
     
     r=minimize(cost_fun, [0], jac=False, method='slsqp',  # slsqp
                       options={'maxiter': 200, 'disp': True }, # maximum iteration number
-                     constraints=({'type':'ineq','fun': pos_upper_bound},
+                     constraints=(
+                         {'type':'ineq','fun': pos_middle_position},
                          {'type':'ineq','fun': pos_lower_bound}
                                   ))
+    
+    print('\n \n',pos_lower_bound(r.x),cost_fun(r.x))
+    
     return r
     
+def remove_old_add_new(point,L,P,X):
+    '''
+    When a point has a torque that is to high to stop into the position limits we remove it from L
+    and send it to R. Then we perform check on P if there is a point that share a similar a x or y position
+    and we move toward it. If there is no point we create a new point initializate with a coordinate shared
+    with the previous point and one on the x or y limit.
+    '''   
+    x0=float(point[0]);
+    y0=float(point[1]);
+    a=[];
+    qmax=X[0][1]
+    vmax=X[1][1]
+    if (len(P)==0):
+        P=[(0,0)]
+        
+    for i in range(len(P)): # check on P is there is a similar point
+            
+        if ( abs(y0) >= abs(x0) ):  # if velocity > position we reduce it  
+                                    # Possible improvement is to rescale accordingly the bound
+            if (x0==P[i][0]):
+                    b=P[i][1]
+                    a.append(b)
+          
+            if (y0>=0.0):
+                if (a==[] and y0==0.0):
+                    a=[-vmax]
+                elif(y0>=0.0):
+                    a=[0.0]
+                max_v= max(a)
+                
+                new_point=(x0,(y0+max_v)/2.00)
+            else:
+                if (a==[] and y0==0.0):
+                    a=[vmax]
+                elif(y0<=0.0):
+                    a=[0.0]
+                max_v= min(a)
+                
+                new_point=(x0,(y0+max_v)/2.00)
+                  
+        else:
+            print("else")
+            if (y0==P[i][1]):
+                    b=P[i][0]
+                    a.append(b)
+                                    
+            if (x0>=0.0):
+                if (a==[] and x0==0.0):
+                    a=[-qmax]
+                elif(x0>=0.0):
+                    a=[0.0]
+                max_v= max(a)
+                new_point=((x0+max_v)/2.00,y0)
+            else:
+                if (a==[] and y0==0.0):
+                    a=[qmax]
+                elif(x0<=0.0):
+                    a=[0.0]
+                max_v= min(a)
+                new_point=((x0+max_v)/2.00,y0)
+                
+    L.remove((x0,y0))   # We remove the point from L
+    L.append(new_point) # and add the new one
+    return 
 
+def remove_old_add_new_if_P(point,L,R,X):
+    '''
+    Same as remove_old_add_new() but with for positively evaluated number. 
+    We also add to L a new_point_2, but it is probably to check again if is it useful
+    '''    
+    x0=float(point[0]);
+    y0=float(point[1]);
+    a=[];
+    qmax=X[0][1]
+    vmax=X[1][1]
+    for i in range(len(R)):
+            
+    
+        if ( abs(y0) >= abs(x0) ):
+            
+            if (x0==R[i][0]):
+                    b=R[i][1]
+                    a.append(b)
+                    
+            # if (a==[]):
+            #     a=[vmax]
+                       
+            if (y0>=0.0):
+                if (a==[]):
+                    a=[vmax]
+                max_v= max(a)
+                new_point=(x0,(y0+max_v)/2.00)
+                new_point2=(np.sign(x0)*np.random.random(1)*vmax,(y0+max_v)/2.00)
+                
+            else:
+                if (a==[]):
+                    a=[-vmax]
+                max_v= min(a)
+                new_point=(x0,(y0+max_v)/2.00)
+                new_point2=(np.sign(x0)*np.random.random(1)*vmax,(y0+max_v)/2.00)  
+                
+        elif ( abs(y0) <= abs(x0) ):
+            
+            if (y0==R[i][1]):
+                    b=R[i][0]
+                    a.append(b)
+                    
+            # if (a==[]):
+            #     a=[-np.sign(x0)*qmax]
+                            
+            if (x0>=0.0):
+                if (a==[]):
+                    a=[vmax]
+                max_v= max(a)
+                new_point=((x0+max_v)/2.00,y0)
+                new_point2=((x0+max_v)/2.00,np.sign(y0)*np.random.random(1)*qmax)
+            else:
+                if (a==[]):
+                    a=[-vmax]
+                max_v= min(a)
+                new_point=((x0+max_v)/2.00,y0)
+                new_point2=((x0+max_v)/2.00,np.sign(y0)*np.random.random(1)*qmax)
+    
+    L.remove((x0,y0))
+    L.append(new_point)
+    L.append(new_point2)
+    return 
 
+def rejected_point(point,L,P,X):
+    x0=point[0];
+    y0=point[1];
+    Pp=[0.0];
+  
+    if ( abs(y0) >= abs(x0) ):
+        
+        for i in range(len(P)):
+            if (x0==P[i][0]):
+                    b=P[i][1]
+                    Pp.append(b)
+                    
+        P_vec = [None]*len(Pp)           
+        for i in range(len(Pp)):
+            P_vec[i]=abs(y0-Pp[i])
+            
+        P_max=min(P_vec)
+        ind=P_vec.index(P_max)
+        P_eff=Pp[ind]
+                        
+        new_point= (x0,(y0+P_eff)/2.0)
+    elif ( abs(x0) >= abs(y0) ):
+        for i in range(len(P)):
+            if (y0==P[i][1]):
+                    b=P[i][0]
+                    Pp.append(b)
+                    
+        P_vec = [None]*len(Pp)           
+        for i in range(len(Pp)):
+            P_vec[i]=abs(x0-Pp[i])
+            
+            
+        P_max=min(P_vec)
+        ind=P_vec.index(P_max)
+        P_eff=Pp[ind]
+                        
+        new_point= ((x0+P_eff)/2.0,y0)    
 
+    L.remove((x0,y0))
+    L.append(new_point)
+    return
+
+def Approved_point(point,L,P,X):
+    x0=point[0];
+    y0=point[1];
+    
+    pos_y= X[1][1]
+    pos_x= X[0][1]
+    neg_y= X[1][0]
+    neg_x= X[0][0]
+  
+    if ( abs(y0) >= abs(x0) ):
+        if (y0>=0):
+            Pp=[pos_y/1.5];
+        else:
+            Pp=[neg_y/1.5];
+            
+        for i in range(len(P)):
+            if (x0==P[i][0]):
+                    b=P[i][1]
+                    Pp.append(b)
+                    
+        P_vec = [None]*len(Pp)           
+        for i in range(len(Pp)):
+            P_vec[i]=abs(y0-Pp[i])
+            
+        P_max=min(P_vec)
+        ind=P_vec.index(P_max)
+        P_eff=Pp[ind]
+                        
+        new_point= (x0,(y0+P_eff)/2.0)
+    elif ( abs(x0) >= abs(y0) ):
+        if (x0>=0):
+            Pp=[pos_x/1.5];
+        else:
+            Pp=[neg_x/1.5];
+            
+        for i in range(len(P)):
+            if (y0==P[i][1]):
+                    b=P[i][0]
+                    Pp.append(b)
+                    
+        P_vec = [None]*len(Pp)           
+        for i in range(len(Pp)):
+            P_vec[i]=abs(x0-Pp[i])
+            
+            
+        P_max=min(P_vec)
+        ind=P_vec.index(P_max)
+        P_eff=Pp[ind]
+                        
+        new_point= ((x0+P_eff)/2.0,y0)    
+
+    L.remove((x0,y0))
+    L.append(new_point)
+    return
 ######################################################################
 # Here there are functions that are not in use and possibly not correct
 # To be removed
 ###################################################################### 
+# def remove_old_add_new(point,L,q):
+    
+#     x0=point[0];
+#     y0=point[1];
+#     a=[];
+    
+#     for i in range(len(L)):
+#             if (x0==L[i][0]):
+#                 b=[L[i][0]]
+#                 a.append(b)
+    
+#     if ( abs(y0) >= abs(x0) ):
+        
+#         if (y0>0):
+#             max_v= np.amax(a)
+#             new_point=np.array([[x0,(y0-max_v)/2]])
+#         else:
+#             max_v= np.amin(a)
+#             new_point=np.array([[x0,(y0-max_v)/2]])   
+#     else:
+        
+#         if (y0>0):
+#             max_v= np.amax(a)
+#             new_point=np.array([[(x0-max_v)/2,y0]])
+#         else:
+#             max_v= np.amin(a)
+#             new_point=np.array([[(x0-max_v)/2,y0]])
+            
+#     ind=np.where(L==point)
+#     new_L = np.delete(L,q)
+#     new_L =np.concatenate((L,new_point))
+#     return new_L 
+ 
   
 # def Compute_torque(X,U,L_point,dt,nonlinear,M):
 
