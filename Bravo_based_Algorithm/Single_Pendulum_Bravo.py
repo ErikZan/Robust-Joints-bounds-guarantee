c2b505a9f8916ebea513c29c7165f06e93b54cf2
Erik <erikzanolli@gmail.com>
2021-01-18 17:29:36 +0100

    Added lower_acc bound

--- Bravo_based_Algorithm/Single_Pendulum_Bravo.py
+++ Bravo_based_Algorithm/Single_Pendulum_Bravo.py
@@ -24,8 +24,8 @@ from scipy.optimize import minimize
 m=1.5 
 l=2.0
 g=9.81
-torque=[-30.0,30.0]
-q_interval=[0.0,3.14/2]
+torque=[-10.0,10.0]
+q_interval=[0,3.14/2]
 
 #### Function ####
 def max_acc(Xi,tau): # interval aritmetic ?
@@ -38,24 +38,31 @@ def max_acc(Xi,tau): # interval aritmetic ?
     
     return [ddq_min,ddq_max]
     
-####  ####
+#### Initializate step_size and Max_acc arrays ####
 step_size=0.05
 L=np.arange(q_interval[0],q_interval[1],step_size)
+L2=np.arange(q_interval[0],q_interval[1],step_size)
 Acc_min=np.zeros(size(L))
 Acc_max=np.zeros(size(L))
 
+print('n             Acc_min               Acc_max')
 for i in range(size(L)-1):
     Acc_min[i]=max_acc((L[i],L[i+1]),torque)[0]
     Acc_max[i]=max_acc((L[i],L[i+1]),torque)[1]
     
     print(i,Acc_min[i],Acc_max[i])
     
-#### Maximize are of viable squared area ###
+#### Maximize are of viable squared area: using a fixed step of 0.05 ###
 q_from_optimization=np.zeros(size(L))
 dq_from_optimization=np.zeros(size(L))
 
 for i in range(size(L)-1):
     def area(q):
+        '''
+        Area defined as q*dq where dq is already in the form of 
+        dq=sqrt(2*abs(acc)*((qmax-qmin)-(q-qmin))) 
+        and limits are given only on positions
+        '''
         qmin=L[i]
         qmax=L[i+1]
         acc=Acc_min[i]
@@ -76,19 +83,30 @@ for i in range(size(L)-1):
                             {'type':'ineq','fun': q_limit_pos}
                                     ))
     print(r.x)
-    (q_from_optimization[i],dq_from_optimization[i])=(r.x,sqrt(2*abs(r.x)*((L[i+1]-L[i])-(r.x-L[i]))))
+    
+    # 
+    (q_from_optimization[i],dq_from_optimization[i])=(r.x,sqrt(2*abs(Acc_min[i])*((L[i+1]-L[i])-(r.x-L[i]))))
 
 for i in range(size(q_from_optimization)):
     print(q_from_optimization[i],dq_from_optimization[i])
     
-    
+#### Maximize are of viable squared area: using variable step
+# in order to create contiguos areas ###    
+
 for i in range(size(L)-2):
+    
     def area(q):
         qmin=L[i]
         qmax=L[i+1]
         acc=Acc_min[i]
         return -((q-qmin)*sqrt(2*abs(acc)*((qmax-qmin)-(q-qmin))))
 
+    def area2(q):
+        qmin=L[i]
+        qmax=L[i+1]
+        acc=Acc_max[i]
+        return -((q-qmin)*sqrt(2*abs(acc)*((qmax-qmin)-(q-qmin))))
+    
     def q_limit(q):
         qmin=L[i]
         return q-qmin
@@ -104,28 +122,88 @@ for i in range(size(L)-2):
                             {'type':'ineq','fun': q_limit_pos}
                                     ))
     print(r.x)
-    (q_from_optimization[i],dq_from_optimization[i])=(r.x,sqrt(2*abs(r.x)*((L[i+1]-L[i])-(r.x-L[i]))))
+    
+    # Modify the next interval in X_i according with the q derived from optimization
+    
+    (q_from_optimization[i],dq_from_optimization[i])=(r.x,np.sign(Acc_min[i])*-sqrt(2*abs(Acc_min[i])*((L[i+1]-L[i])-(r.x-L[i])))) # if area()
+    #(q_from_optimization[i],dq_from_optimization[i])=(r.x,-sqrt(2*abs(r.x)*((L[i+1]-L[i])-(r.x-L[i])))) # if area2()  
+    
     L[i+1]=q_from_optimization[i]
     L[i+2]=q_from_optimization[i]+step_size
     
 for i in range(size(q_from_optimization)):
     print(q_from_optimization[i],dq_from_optimization[i])
+
+
+# #### Maximize are of viable squared area: using variable step
+# # in order to create contiguos areas : Negative Positions! ### 
+q_from_optimization2=np.zeros(size(L))
+dq_from_optimization2=np.zeros(size(L))
+
+for i in range(size(L2)-2):
+    
+    def area(q):
+        qmin=L2[i]
+        qmax=L2[i+1]
+        acc=Acc_min[i]
+        return -((q-qmin)*sqrt(2*abs(acc)*((qmax-qmin)-(q-qmin))))
+
+    def area2(q):
+        qmin=L2[i]
+        qmax=L2[i+1]
+        acc=Acc_max[i]
+        return -((q-qmin)*sqrt(2*abs(acc)*((qmax-qmin)-(q-qmin))))
+    
+    def q_limit(q):
+        qmin=L2[i]
+        return q-qmin
+
+    def q_limit_pos(q):
+        qmax=L2[i+1]
+        return qmax-q
+
+    r = minimize(area2,L2[i], jac=False, method='slsqp',  # slsqp
+                        options={'maxiter': 200, 'disp': True },#, # maximum iteration number
+                        constraints=(
+                            {'type':'ineq','fun': q_limit},
+                            {'type':'ineq','fun': q_limit_pos}
+                                    ))
+    print(r.x)
+    
+    # Modify the next interval in X_i according with the q derived from optimization
+    
+    (q_from_optimization2[i],dq_from_optimization2[i])=(r.x,np.sign(Acc_max[i])*-sqrt(2*abs(Acc_max[i])*((L[i+1]-L[i])-(r.x-L[i])))) # if area()
+    #(q_from_optimization[i],dq_from_optimization[i])=(r.x,sqrt(2*abs(r.x)*((L[i+1]-L[i])-(L[i]-r.x)))) # if area2()  
+    
+    L2[i+1]=q_from_optimization2[i]
+    L2[i+2]=q_from_optimization2[i]+step_size
+
+for i in range(size(q_from_optimization2)):
+    print(q_from_optimization2[i],dq_from_optimization2[i])
+# Plot stuffs
     
 (f,ax) = plut.create_empty_figure(1)
 LW=1
 
 def square_drawer(qmin,qmax,dqmin,dqmax):
-    ax.plot([qmin, qmax], [0, 0], 'r--',linewidth=LW);
+    ax.plot([qmin, qmax], [dqmin, dqmin], 'r--',linewidth=LW);
     ax.plot([qmin, qmax], [dqmax, dqmax], 'r--',linewidth=LW);
-    ax.plot([qmin, qmin], [0, dqmax], 'r--',linewidth=LW);
-    ax.plot([qmax, qmax], [0, dqmax], 'r--',linewidth=LW);
+    ax.plot([qmin, qmin], [dqmin, dqmax], 'r--',linewidth=LW);
+    ax.plot([qmax, qmax], [dqmin, dqmax], 'r--',linewidth=LW);
 
 scatter(q_from_optimization[:],dq_from_optimization[:],color="green")
+scatter(q_from_optimization2[:],dq_from_optimization2[:],color="blue")
 
+# Draw X_i ranges
+q_from_optimization = np.insert(q_from_optimization, 0, 0.0, axis=0)
+q_from_optimization2 = np.insert(q_from_optimization, 0, 0.0, axis=0)
 for i in range(size(q_from_optimization)-1):
-    square_drawer(q_from_optimization[i],q_from_optimization[i+1],dq_from_optimization[i],dq_from_optimization[i+1])
+    square_drawer(q_from_optimization[i],q_from_optimization[i+1],0,dq_from_optimization[i])
+    square_drawer(q_from_optimization2[i],q_from_optimization2[i+1],0,dq_from_optimization2[i])
 
+# Add accelerations_as_a_text to points
 for i,txt in enumerate(Acc_min):
-     ax.annotate(str(Acc_min[i]), xy=(q_from_optimization[i],dq_from_optimization[i]),size=10)
+    ax.annotate(str(Acc_min[i]), xy=(q_from_optimization[i],dq_from_optimization[i]),size=10)
+    ax.annotate(str(Acc_max[i]), xy=(q_from_optimization2[i],dq_from_optimization2[i]),size=10)
 
 plt.show()
